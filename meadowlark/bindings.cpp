c7df001bdd0bc8ec408a217f5ee956d6f6ccb0cb
Charlie Clarke <claclark@gmail.com>
2025-06-30 17:45:06 +0000

    Hopper iterator

--- meadowlark/bindings.cpp
+++ meadowlark/bindings.cpp
@@ -1,9 +1,32 @@
 #include "src/warren.h"
 #include <pybind11/pybind11.h>
 
+namespace {
+struct HopperIter {
+  cottontail::Hopper *hopper;
+  cottontail::addr k = cottontail::minfinity + 1;
+
+  HopperIter(cottontail::Hopper *h) : hopper(h) {}
+
+  pybind11::object next() {
+    cottontail::addr p, q;
+    cottontail::fval v;
+    hopper->tau(k, &p, &q, &v);
+    if (p >= cottontail::maxfinity)
+      throw pybind11::stop_iteration();
+    k = p + 1;
+    return pybind11::make_tuple(p, q, v);
+  }
+};
+} // namespace
+
 PYBIND11_MODULE(_meadowlark, m) {
+  m.doc() = "Meadowlark: Python wrapper for Cottontail annotative indexing";
   m.attr("maxfinity") = pybind11::int_(cottontail::maxfinity);
   m.attr("minfinity") = pybind11::int_(cottontail::minfinity);
+  pybind11::class_<HopperIter>(m, "HopperIter")
+      .def("__iter__", [](HopperIter &self) -> HopperIter & { return self; })
+      .def("__next__", &HopperIter::next);
   pybind11::class_<cottontail::Hopper, std::unique_ptr<cottontail::Hopper>>(
       m, "Hopper")
       .def("tau",
@@ -27,13 +50,17 @@ PYBIND11_MODULE(_meadowlark, m) {
              self.uat(k, &p, &q, &v);
              return pybind11::make_tuple(p, q, v);
            })
-      .def("ohr", [](cottontail::Hopper &self, cottontail::addr k) {
-        cottontail::addr p, q;
-        cottontail::fval v;
-        self.ohr(k, &p, &q, &v);
-        return pybind11::make_tuple(p, q, v);
-      });
-
+      .def("ohr",
+           [](cottontail::Hopper &self, cottontail::addr k) {
+             cottontail::addr p, q;
+             cottontail::fval v;
+             self.ohr(k, &p, &q, &v);
+             return pybind11::make_tuple(p, q, v);
+           })
+      .def(
+          "__iter__",
+          [](cottontail::Hopper &self) { return HopperIter(&self); },
+          pybind11::keep_alive<0, 1>());
   pybind11::class_<cottontail::Warren, std::shared_ptr<cottontail::Warren>>(
       m, "Meadow")
       .def_static("open",
--- test.py
+++ test.py
@@ -4,21 +4,15 @@ import argparse
 import  meadowlark as lark
 
 def main():
-    parser = argparse.ArgumentParser(description="Print Meadow metadata")
+    parser = argparse.ArgumentParser(description="Test Meadowlark")
     parser.add_argument("--burrow", required=True, help="Path to the burrow")
     args = parser.parse_args()
 
     try:
         meadow = lark.Meadow.open(args.burrow)
         with meadow:
-            hopper = meadow.hopper("hello world")
-            k = 0
-            while True:
-                p, q, _ = hopper.tau(k)
-                if p == lark.maxfinity:
-                    break
+            for p, q, _ in meadow.hopper("hello world"):
                 print(p, q)
-                k = p + 1
     except RuntimeError as e:
         print(e)
 
